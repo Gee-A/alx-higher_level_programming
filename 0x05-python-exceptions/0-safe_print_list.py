b87a1ad4e4f15b164ec3b42e9f956bce7815127c
Gee-A <gideonadebowale66@gmail.com>
2022-07-18 10:51:35 +0000

    removed the except conditions as to suit task given

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	count = 0
-	for i in range(0, x):
-		try:
-			print("{:d}".format(my_list[i]), end="")
-		except IndexError:
-			pass
-		else:
-			count += 1
-	print()
-	return count
-		
-
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except:
+            pass
+    print()
+    return count
