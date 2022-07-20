40fa072cc055e1e9c59eaa8f89dad689740e58b1
Gee-A <gideonadebowale66@gmail.com>
2022-07-20 01:42:32 +0100

    added area function to the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Defining a Square class """
+
+
+class Square:
+    """ Represent an empty square class """
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        """ returns the current square area """
+        return self.size ** 2
