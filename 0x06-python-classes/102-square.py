df9688f59636fe473320ed96f988321b92bc3cc3
Gee-A <gideonadebowale66@gmail.com>
2022-07-20 03:47:23 +0000

    a little fix

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 
 """ Defining a Square class """
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 """Define a MagicClass matching exactly a bytecode provided by Holberton."""
-
-
 import math
 
 
@@ -14,15 +12,15 @@ class MagicClass:
         Arg:
             radius (float or int): The radius of the new MagicClass.
         """
-        self.__radius = 0
+        self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        self.__radius = radius
+        self._MagicClass__radius = radius
 
     def area(self):
         """Return the area of the MagicClass."""
-        return (self.__radius ** 2 * math.pi)
+        return (self._MagicClass__radius ** 2 * math.pi)
 
     def circumference(self):
         """Return the circumference of the MagicClass."""
-        return (2* math.pi * self.__radius)
+        return (2 * math.pi * self._MagicClass__radius)
