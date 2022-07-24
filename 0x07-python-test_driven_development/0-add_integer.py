44d2e416101215ab993e973d7811400bef38247a
Adebowale Gideon <gideonadebowale66@gmail.com>
2022-07-24 02:34:19 +0100

    added the add_integer and README file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+# (Remove this at the end)!/usr/bin/python3
+"""Module add_integer adds two numbers to return an integer"""
+
+
+def add_integer(a, b=98):
+    """
+    Return the addition of a and b, or error
+    if a or b is not of int or float type.
+    Args:
+        a: first number
+        b: second number
+    """
+
+    if type(a) is not float and type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not float and type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,7 @@
+# 0x07. Python - Test-driven development
+
+## Table of Content
+
+Files | Description
+------|------------
+[Test folder](./tests)| contains all test files in .txt extension
