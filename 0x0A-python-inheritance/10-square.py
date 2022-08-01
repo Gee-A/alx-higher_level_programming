bfaac2b240e761ea0ed2314bf5ce4aa5fe14e548
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-08-01 23:16:46 +0100

    added square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Module 10-rectangle.py
+Inherits from Rectangle class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square.
+    Private instance attributes:
+        -size
+    Inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """Initializes an instance of Square class
+        Args:
+            size: size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Utilizing Rectangle string method"""
+        return super().__str__()
+
+    def area(self):
+        """Computes the area of the Square instance.
+        Overwrites the area() method from Rectangle.
+        """
+        return self.__size ** 2
