512d2d0a373d61152ea4af977ad493e599011ad8
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-08-02 15:24:11 +0100

    added task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Module 4-from_json_string
+Returns an object represented by a JSON string."""
+import json
+
+
+def from_json_string(my_str):
+    """Return the object represented by my_str.
+    Args:
+        my_str: JSON string representation
+    Returns: corresponding object
+    """
+    return json.loads(my_str)
