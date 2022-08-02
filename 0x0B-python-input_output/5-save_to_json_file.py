86471928ce7cdeb678f19a28bbf09271f720304b
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-08-02 15:35:04 +0100

    added task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Module 5-save_to_json_file
+Writes an Object to a text file, using a JSON representation.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes JSON representation dumps into filename
+    Args:
+        my_obj: object to write
+        filename: file to write into
+    """
+    text = json.dumps(my_obj)
+    with open(filename, 'w') as f:
+        f.write(text)
