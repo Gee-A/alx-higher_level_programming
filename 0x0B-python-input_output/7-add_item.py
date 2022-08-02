daa2dff97bf684acbabcc59781dd50a8c774240d
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-08-02 16:19:35 +0100

    a quick fix to add_item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@
 A python script that adds all arguments to a Python list,
 and then save them to a file"""
 
+import json
 import sys
 import os.path
 
@@ -11,6 +12,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_file = "add_item.json"
+
 my_list = []
 
 if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
