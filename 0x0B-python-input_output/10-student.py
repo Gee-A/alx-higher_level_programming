64c6fdb530ae0759665932fc99b26c9360ff2447
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-08-02 17:04:22 +0100

    added task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Module 9-student.py
+Creates a Student class."""
+
+
+class Student:
+    """Defines a student
+    Public attributes:
+        - first_name
+        - last_name
+        - age
+    Public method: to_json()
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a student instance"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance.
+
+        Returns: the dict representation of the instance.
+        """
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
