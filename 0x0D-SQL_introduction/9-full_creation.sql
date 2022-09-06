23192e5ae1e33d7c1660d8b6a1251226ca06a258
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-09-07 00:05:29 +0100

    [ADD] task 12

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,7 @@
 -- creates a new table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- adds new rows to the table
-INSERT INTO second_table 
-VALUES (1, 'John', 10);
-VALUES (2, 'Alex', 3);
-VALUES (3, 'Bob', 14);
-VALUES (4, 'George', 8);
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);
