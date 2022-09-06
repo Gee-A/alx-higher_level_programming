2cd2c78b5f5ae4346b6f1873632dde73dc7bed9f
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-09-06 23:23:03 +0100

    [FIX] task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,6 @@
 -- creates a table in the database in MySQL server
 CREATE second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table
-(id, name, score)
 VALUES (1, 'John', 10)
 VALUES (2, 'Alex', 3)
 VALUES (3, 'Bob', 14)
