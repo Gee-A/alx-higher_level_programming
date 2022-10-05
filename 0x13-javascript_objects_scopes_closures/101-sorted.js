e4498e069b52b3eadb5642b630c9a7c5aad45f6e
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-05 06:00:43 +0530

    corrected required directory on task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const { dict } = require('./main.js');
+const { dict } = require('./101-data.js');
 
 const newDict = Object.entries(dict).reduce((acc, [key, value]) => {
   acc[value] = acc[value] ? [...acc[value], key] : [key];
