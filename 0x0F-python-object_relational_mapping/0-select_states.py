865e3e9114fabc3c2970757bcdaf4ce61c3ee218
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-06 14:05:21 +0100

    added task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Connects to a database and execute a query using a python script.
+"""
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
+                     db=sys.argv[3], charset='utf8')
+
+cur = db.cursor()
+cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+records = cur.fetchall()
+for record in records:
+    print(record)
