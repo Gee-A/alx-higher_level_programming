270ba477f504e1c9c1637dd7e6ef6bbc89932630
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-06 14:37:27 +0100

    added task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""script lists all staes with a name starting with 'N' from the database.
+"""
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+
+cur = db.cursor()
+cur.execute("""SELECT * FROM states WHERE states.name
+            LIKE 'N%' ORDER BY states.id ASC""")
+records = cur.fetchall()
+for record in records:
+    print(record)
