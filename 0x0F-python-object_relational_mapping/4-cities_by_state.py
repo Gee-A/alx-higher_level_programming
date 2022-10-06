e468ed9826aef0c9c7724adb535c23a2456380e2
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-06 16:14:35 +0100

    added task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A python script that lists all cities from the database.
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name\
+                FROM cities c INNER JOIN states s\
+                ON c.state_id = s.id")
+    records = cur.fetchall()
+    for record in records:
+        print(record)
