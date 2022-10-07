ebfd027bad9b294f14c835995d3b6cf1739cad82
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-08 04:51:33 +0530

    fixed file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
 
-    string = "SELECT * FROM states WHERE states.name = '{}'\
-            ORDER BY states.id ASC".format(sys.argv[4])
     cur = db.cursor()
-    cur.execute(string)
+    cur.execute("SELECT * FROM states\
+                WHERE states.name = '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     records = cur.fetchall()
     for record in records:
         print(record)
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
 
-    # I haven't gotten the sql injection free syntax
     cur = db.cursor()
     cur.execute("SELECT c.name\
-                FROM cities c LEFT JOIN states s\
+                FROM cities c INNER JOIN states s\
                 ON c.state_id = s.id\
-                WHERE s.name =%s\
+                WHERE s.name = %s\
                 ORDER BY c.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+    records = cur.fetchall()
+    print(", ".join([record[0] for record in records]))
+
     cur.close()
     db.close()
