5766b7532fca06dc353ea312b98a8b85ab1b2b57
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-10 06:21:35 +0530

    ensure collate is in latin1_general_cs

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,14 +3,16 @@
 """
 
 if __name__ == "__main__":
-    import MySQLdb
     import sys
+    import MySQLdb
 
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name\
+    cur.execute("SELECT * FROM states\
+                WHERE CONVERT(states.name USING Latin1)\
+                COLLATE Latin1_General_CS\
                 LIKE 'N%' ORDER BY states.id ASC")
     records = cur.fetchall()
     for record in records:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,12 @@ if __name__ == "__main__":
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+                         passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                WHERE states.name = '{}'\
+                WHERE CONVERT(states.name USING Latin1)\
+                COLLATE Latin1_General_CS = '{}'\
                 ORDER BY states.id ASC".format(sys.argv[4]))
     records = cur.fetchall()
     for record in records:
