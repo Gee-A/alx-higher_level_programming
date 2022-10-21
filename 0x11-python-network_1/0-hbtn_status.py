1f556ae330eeb30aa444c64e989f0b234e678eb6
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-22 00:28:57 +0100

    fixed typo in task 0 file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,5 +11,4 @@ if __name__ == "__main__":
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t- ut8 content: {}'.format(html.decode("utf-8", "replace")))
-
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
