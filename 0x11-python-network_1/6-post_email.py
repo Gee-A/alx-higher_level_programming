ac4fefaf7d9d3e4e72e7c73080a3a41cac7e0e91
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-22 02:16:13 +0100

    ensures module wont run if imported

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,6 @@ and finally displays the boody of the response
 import requests
 import sys
 
-res = requests.post(sys.argv[1], data={'email': sys.argv[2]})
-print(res.text)
+if __name__ == "__main__":
+    res = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(res.text)
