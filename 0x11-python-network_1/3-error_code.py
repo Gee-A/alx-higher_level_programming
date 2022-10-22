c2eedaef563494edae3b70c0cd6e6fbcef7d13df
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-22 01:41:02 +0100

    fixed task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8', "replace"))
     except urllib.request.HTTPError as e:
-        print('Error code: ', e.code)
+        print('Error code: {}'.format(e.code))
