59ae25aaba62d1932b794cb2235777d788f68d76
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-10-22 03:09:09 +0100

    a guessed fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    q = sys.argv[1] if len(sys.argv) > 1 and else ""
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
     res = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
         dic = res.json()
@@ -16,4 +16,4 @@ if __name__ == "__main__":
         else:
             print('[{}] {}'.format(dic.get('id'), dic.get('name')))
     except ValueError:
-        print('No result')
+        print('Not a valid JSON')
