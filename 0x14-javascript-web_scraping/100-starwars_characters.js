9c3b7af15c4d402fc121a4418feb9e1e3e51597b
AdebowaleGideon <gideonadebowale66@outlook.com>
2022-11-16 17:47:08 +0530

    script prints all character of a Star WArs movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
-reqeust.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-	if (res.statusCode === 200) {
-		for (const character of JSON.parse(body).characters) {
-			request.get(character, function (err1, res1, body1) {
-				if (res1.statusCode === 200) {
-					console.log(JSON.parse(body1).name);
-				} else if (err1) {
-					console.log(err1);
-				}
-			});
-		}
-	} else if (err) {
-		console.log(err);
-	}
+request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
+  if (res.statusCode === 200) {
+    for (const character of JSON.parse(body).characters) {
+      request.get(character, function (err1, res1, body1) {
+        if (res1.statusCode === 200) {
+          console.log(JSON.parse(body1).name);
+        } else if (err1) {
+          console.log(err1);
+        }
+      });
+    }
+  } else if (err) {
+    console.log(err);
+  }
 });
